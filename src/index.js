c52192bd89ba6aadad7bdbc1fbc2b313ce90f75e
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-04-04 15:28:56 +0200

    PoC - isolating <a> links and their names (untested)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const parser = require('./service/parser');
 
 parser.read_newsletter("src/assets/newsletters/Gmail - GTC22 Event Recap.html", (data) => {
     parser.find_images(data, (images) => {
-        console.log(images);
-    })
+        //console.log(images);
+    });
+    parser.isolate_link_tags(data, (links) => {
+        console.log("\n\n\n\n\n\n");
+        parser.get_link_names(links, (tags) => {
+            console.log(tags);
+        });
+    });
 });
--- src/service/parser.js
+++ src/service/parser.js
@@ -1,12 +1,8 @@
 const fs = require('fs');
 
 function read_newsletter(filename, callback){
-    console.log("Method start!");
     fs.readFile(filename, (err, data) => {
-        if (err) {
-            console.log("Method error!");
-        }
-        console.log("Method end!");
+        if (err) throw err;
         callback(data.toString());
     });
 }
@@ -25,7 +21,79 @@ function find_images(data, callback){
     callback(images);
 }
 
+function clear_subtag(tag){
+    console.log("\n\n\n");
+    split = tag.split("<");
+    next = "";
+    if(split[0] == ""){
+        for(let s=1; s<split.length; s++){
+            next = next + split[s];
+            if(s < split.length-1){
+                next = next + "<";
+            }
+        }
+    }
+
+    split = next.split(">");
+    next = "";
+    for(let s=1; s<split.length; s++){
+        next = next + split[s];
+        if(s < split.length-1){
+            next = next + ">";
+        }
+    }
+    
+    return next;
+}
+
+function clean_link_tag(tag){
+    a = tag.split("<a")[1];
+    na = a.split(">");
+    nota = "";
+    for(let x=1; x<na.length; x++){
+        nota = nota + na[x] + ">";
+    }
+    tagNotA = nota.split("</a")[0];
+
+    clean = false;
+    while(!clean){
+        if(!tagNotA.includes("<")){
+            clean = true;
+        }else{
+            tagNotA = clear_subtag(tagNotA);
+        }
+    }
+    return tagNotA;
+}
+
+function isolate_link_tags(data, callback){
+    isolating = data.split('<a');
+    links = [];
+    for(l in isolating){
+        if(l>0){
+            links.push("<a"+isolating[l].split("</a>")[0]+"</a>");
+        }
+    }
+    callback(links);
+}
+
+function get_link_names(data, callback){
+    tags = [];
+    for(link in data){
+        let n = clean_link_tag(data[link]);
+        if(n != ""){
+            tags.push({
+                name: n,
+                link: data[link],
+            });
+        }
+    }
+    callback(tags);
+}
+
 module.exports = {
     read_newsletter,
     find_images,
+    isolate_link_tags,
+    get_link_names,
 }
