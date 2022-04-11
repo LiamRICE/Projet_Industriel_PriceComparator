100a1ab065f380f50b6ec21feb988069707fc601
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-04-11 15:35:03 +0200

    PoC - added loop to make simpler API call to store images in server, still untested

--- src/experiment.js
+++ src/experiment.js
@@ -2,6 +2,7 @@ const parser = require('./service/parser');
 const image = require('./service/image');
 const fs = require('fs');
 
+// TODO - make data an array of image_data (like below) with the correct information.
 data = {
   company_name: "Nvidia",
   image_url: "https://ci5.googleusercontent.com/proxy/A5orN-QdXXvb3dNlSCHmmkrRW5904qntUEzaxuVy9jOBNlI0e0hBq3DIC_XFMsSrL16gLOSMEQnAtcVei52hRiSIOoqSTIgau6ot=s0-d-e1-ft#https://info.nvidia.com/rs/156-OFN-742/images/spacer.gif",
--- src/service/image.js
+++ src/service/image.js
@@ -1,28 +1,38 @@
 import axios from 'axios';
 
-export async function get_image(image_data) {
-    let image_type = "newsletter_images";
-    let account_name = image_data.company_name;
-    let image_url = image_data.image_url;
-    let product_id = get_unique_ID();
-    return new Promise((resolve, reject) => {
-        try {
-            const url = `185.63.174.6:9600/image/${image_type}/${account_name}/${image_url}/${product_id}`;
-            const config = {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json' 
-                },
-            };
-            axios.get(url, config).then((result) => {
-                resolve(result.data);
-            });
-        } catch (err) {
-            reject(err);
-        }
-    });
+export async function get_image(image_data_list) {
+    let id = get_unique_ID(image_data_list.size());
+    for(image_data in image_data_list){
+        let image_type = "newsletter_images";
+        let account_name = image_data_list[image_data].company_name;
+        let image_url = image_data_list[image_data.image_url];
+        let product_id = id + image_data;
+        return new Promise((resolve, reject) => {
+            try {
+                const url = `185.63.174.6:9600/image/${image_type}/${account_name}/${image_url}/${product_id}`;
+                const config = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json' 
+                    },
+                };
+                axios.get(url, config).then((result) => {
+                    resolve(result.data);
+                });
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
 }
 
-function get_unique_ID(){
-    return 0;
+function get_unique_ID(num_images){
+    fs.readFile('src/assets/data/state.txt', (read) => {
+        let number = parseInt(read);
+        let num = number + num_images;
+        fs.writeFile('src/assets/data/state.txt', num, (err) => {
+            if (err) throw err;
+        })
+        return number;
+    })
 }
