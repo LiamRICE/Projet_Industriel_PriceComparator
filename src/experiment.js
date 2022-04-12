1980bc0a27ed23a3815fd36098c9d05f3843c60b
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-04-12 15:34:10 +0200

    PoC - worked on the newletter image upload service, tested, not functional, additionnal doc required

--- src/experiment.js
+++ src/experiment.js
@@ -3,11 +3,21 @@ const image = require('./service/image');
 const fs = require('fs');
 
 // TODO - make data an array of image_data (like below) with the correct information.
-data = {
+let data1 = {
   company_name: "Nvidia",
   image_url: "https://ci5.googleusercontent.com/proxy/A5orN-QdXXvb3dNlSCHmmkrRW5904qntUEzaxuVy9jOBNlI0e0hBq3DIC_XFMsSrL16gLOSMEQnAtcVei52hRiSIOoqSTIgau6ot=s0-d-e1-ft#https://info.nvidia.com/rs/156-OFN-742/images/spacer.gif",
-}
-let link = image.getImage(data);
+};
+let data2 = {
+  company_name: "Nvidia",
+  image_url: "https://ci3.googleusercontent.com/proxy/rKDtFTQ5yrz_GyIZJDkZecyT6IJzl1GP0V0GeZCfnKU5-AEogf-QQ9EfuODTMhhEX9Xk4V6w4DfiNlYdnsVcN8Hn-s68KgkcyRkXG92whzZuNCWPhoWDFRSR5pFOHuB1Y-Z0WAMD=s0-d-e1-ft#https://info.nvidia.com/rs/156-OFN-742/images/hpc-quantum-computing-organic-600.jpg",
+};
+let data3 = {
+  company_name: "Nvidia",
+  image_url: "https://ci3.googleusercontent.com/proxy/dlzwH2QfVxApw-BGN2zUhGaHzD4QedvItZ8foKF5gN2gXkQ2yzwjaK016-wb5gj4FNEd-FVpIVaYuzHCq_W9NhZ-Va2JrqXo1RN2xiTAtnx7yMVvgpDtVVUdvhHK=s0-d-e1-ft#https://info.nvidianews.com/rs/nvidia/images/nv-social_icons-gaming-fb.png",
+};
+let data = [data1, data2, data3];
+
+let link = image.get_image(data);
 console.log(link);
 
 fs.writeFile('src/assets/output/image-links.txt', link, (err) => {
--- src/service/image.js
+++ src/service/image.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
+const axios = require('axios');
+const fs = require('fs');
 
-export async function get_image(image_data_list) {
-    let id = get_unique_ID(image_data_list.size());
-    for(image_data in image_data_list){
+async function get_image(image_data_list) {
+    //let id = get_unique_ID(image_data_list.length);
+    for(let image_data=0; image_data<image_data_list.length; image_data++){
         let image_type = "newsletter_images";
         let account_name = image_data_list[image_data].company_name;
-        let image_url = image_data_list[image_data.image_url];
-        let product_id = id + image_data;
+        let image_url = image_data_list[image_data].image_url;
+        let product_id = image_data;
+        
         return new Promise((resolve, reject) => {
             try {
                 const url = `185.63.174.6:9600/image/${image_type}/${account_name}/${image_url}/${product_id}`;
+                console.log(url);
                 const config = {
                     method: 'GET',
                     headers: {
@@ -17,6 +20,10 @@ export async function get_image(image_data_list) {
                     },
                 };
                 axios.get(url, config).then((result) => {
+                    console.log('\nData :')
+                    console.log(result.data);
+                    console.log('\nFull result :')
+                    console.log(result)
                     resolve(result.data);
                 });
             } catch (err) {
@@ -28,11 +35,17 @@ export async function get_image(image_data_list) {
 
 function get_unique_ID(num_images){
     fs.readFile('src/assets/data/state.txt', (read) => {
+        //console.log("Read = "+read);
         let number = parseInt(read);
+        //console.log("Number = "+number);
         let num = number + num_images;
         fs.writeFile('src/assets/data/state.txt', num, (err) => {
             if (err) throw err;
         })
         return number;
     })
+}
+
+module.exports = {
+    get_image,
 }
