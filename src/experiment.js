3c866a70333c88001545f7b9d70dbbbe0c4c99e2
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-04-20 09:25:33 +0200

    PoC - advanced on experiment code, service complete and functional

--- src/experiment.js
+++ src/experiment.js
@@ -1,23 +1,29 @@
 const parser = require('./service/parser');
 const image = require('./service/image');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 /*
-let data1 = {
-  company_name: "Nvidia",
-  image_url: "https://ci3.googleusercontent.com/proxy/caQNF0ETo6SDE_zrscoKYKBxlMhqef8ioOVq41uwMV8u3JkKYQYrC3GeG5MLbJWptC1bpDyluJ2SdPSgM9Y8CBpn-DG2X9eenL-I0zUucU7TdzKCs35gviT11tlq5L0hOUK6=s0-d-e1-ft#https://info.nvidia.com/rs/156-OFN-742/images/newsletter-hopper-announcement.jpg",
-};
-
-image.get_image(data1).then((data, err) => {
-  if(err){
-    err.message();
-  }else{
-    let res = data;
-
-    fs.writeFile('src/assets/output/image-links.txt', res, (err) => {
-        if (err) throw err;
+async function process(parsed_data){
+  for(d in parsed_data){
+    image.get_image(parsed_data[d]).then((data, err) => {
+      data = data+'\n';
+      if(err){
+        err.message();
+      }else{
+        try {
+          fs.appendFile('nvidia-output.txt', data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
     });
   }
+}
+
+parser.parse_newsletter("src/assets/newsletters/Gmail - GTC22 Event Recap.html", (data) => {
+  image.to_data(data, (parsed_data) => process(parsed_data));
 });
 */
+
+
 
