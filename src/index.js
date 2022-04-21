7de0ba6c48dc1c2f1c732dc28001c4bd4811d83a
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-04-21 10:35:23 +0200

    PoC - downloads all newsletters and writes them to files

--- src/index.js
+++ src/index.js
@@ -21,42 +21,68 @@ fs.readFile('src/service/credentials.json', (err, content) => {
 async function main(oAuth2Client) {
   const messages = await gmail.listMessages(oAuth2Client, 'label:inbox');
   console.log('Messages:', messages);
-  const id = messages[0].id;
-  console.log(id);
-  const message = await gmail.getMessage(oAuth2Client,id);
-  console.log('Message:',message);
-  const headers = message.data.payload.headers;
-  //console.log(headers);
-  var subject = "";
-  var from = "";
-  var date = "";
-  var data = "";
-  headers.forEach((elem) => {
-    if(elem.name == "Subject"){
-      subject = elem.value;
-      console.log('subject:',subject);
+  for(m in messages){
+    const id = messages[m].id;
+    console.log(id);
+    const message = await gmail.getMessage(oAuth2Client,id);
+    //console.log('Message:',message);
+    const headers = message.data.payload.headers;
+    //console.log(headers);
+    var subject = "";
+    var from = "";
+    var date = "";
+    var data = "";
+    headers.forEach((elem) => {
+      if(elem.name == "Subject"){
+        subject = elem.value;
+        console.log('subject:',subject);
+      }
+      if(elem.name == "From"){
+        from = elem.value;
+        console.log('from:',from);
+      }
+      if(elem.name == "Date"){
+        date = elem.value;
+        console.log('date:',date);
+      }
+    });
+    if(message.data.payload.parts == undefined){
+      if(message.data.payload.mimeType == 'text/html'){
+        data = message.data.payload.body.data;
+      }
+      const txt = Buffer.from(data,'base64').toString('utf8');
+      //console.log("texte:",txt);
+      let n = from.split(' <')[0];
+      if(n.includes('"')){
+        n = n.split('"')[1];
+      }
+      console.log(`${n}.html`);
+      fs.writeFile(`${n}.html`, txt, (err) => {
+        if (err) throw err;
+      });
+      const message2 = await gmail.editMessageLabel(oAuth2Client,id,[],['UNREAD']);
+      //console.log(message2)
+    }else{
+      const parts = message.data.payload.parts;
+      console.log(message.data.payload);
+      parts.forEach((elem) => {
+        if(elem.mimeType == "text/html"){
+          data = elem.body.data;
+          //console.log('data:',data);
+        }
+      });
+      const txt = Buffer.from(data,'base64').toString('utf8');
+      //console.log("texte:",txt);
+      let n = from.split(' <')[0];
+      if(n.includes('"')){
+        n = n.split('"')[1];
+      }
+      console.log(`${n}.html`);
+      fs.writeFile(`${n}.html`, txt, (err) => {
+        if (err) throw err;
+      });
+      const message2 = await gmail.editMessageLabel(oAuth2Client,id,[],['UNREAD']);
+      //console.log(message2)
     }
-    if(elem.name == "From"){
-      from = elem.value;
-      console.log('from:',from);
-    }
-    if(elem.name == "Date"){
-      date = elem.value;
-      console.log('date:',date);
-    }
-  });
-  const parts = message.data.payload.parts;
-  parts.forEach((elem) => {
-    if(elem.mimeType == "text/html"){
-      data = elem.body.data;
-      //console.log('data:',data);
-    }
-  });
-  const txt = Buffer.from(data,'base64').toString('utf8');
-  console.log("texte:",txt);
-  fs.writeFile('test.html', txt, (err) => {
-    if (err) throw err;
-  });
-  const message2 = await gmail.editMessageLabel(oAuth2Client,id,[],['UNREAD']);
-  //console.log(message2)
+  }
 }
