3251f1c7beb5c0eaf9ca6a8b2c8896a437ec9c11
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2022-05-16 13:08:10 +0200

    PoC - moved prototyped code to the service.js service

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const parser = require('./service/parser');
 const image = require('./service/image');
 const gmail = require('./service/gmail');
+const service = require('./service/service');
 const Mutex = require('async-mutex').Mutex;
 const fs = require('fs');
 const {google} = require('googleapis');
@@ -35,7 +36,7 @@ fs.readdir("src/assets/newsletters", (err, files) => {
     });
 });
 */
-
+/*
 let mutex = new Mutex();
 
 async function downloadAll(){
@@ -130,102 +131,6 @@ async function uploadImages(data){
         });
     });
 }
-
-
-/*
-async function downloadAll(){
-    console.log("Fetching...");
-    gmail.getAllEmails((res) => {
-        console.log(res.length);
-        console.log("Parsing...");
-        for(mail in res){
-            let email = res[mail];
-            parser.parse_newsletter(email.data, email.company, (data) => {
-                image.to_data(data, (formattedData) => {
-                    var newImageLinks = [];
-                    var promise = new Promise((resolve, reject) => {
-                        try{
-                            for(imageData in formattedData){
-                                if(formattedData[imageData] == undefined){
-                                    if (imageData == formattedData.length-1) {
-                                        console.log(`Finished Image Upload : ${mail}`);
-                                        resolve();
-                                    };
-                                }else{
-                                    let linkObject = {
-                                        oldLink : formattedData[imageData].image_url,
-                                        newLink : formattedData[imageData].image_url,
-                                    }
-                                    image.get_image(formattedData[imageData]).then((newLink) => {
-                                        linkObject.newLink = newLink;
-                                        newImageLinks.push(linkObject);
-                                        //console.log(`LINK GENERATED : ${newLink}`);
-                                        if (imageData == formattedData.length-1) {
-                                            console.log(`Finished Image Upload : ${mail}`);
-                                            resolve();
-                                        };
-                                    }).catch((err) => {
-                                        //console.log(`ERROR : ${err}`);
-                                        if (imageData == formattedData.length-1) {
-                                            console.log(`Finished Image Upload : ${mail}`);
-                                            resolve();
-                                        };
-                                    });
-                                }
-                            }
-                        }catch(err){
-                            console.log("UPLOAD FAILED !")
-                            resolve();
-                            reject(err);
-                        }
-                    }).then(() => console.log("THEN")).finally(() => console.log("FINALLY"));
-                    promise.then(() => {
-                        console.log(`Replacement : ${mail}`);
-                        var nPromise = new Promise((resolve, reject) => {
-                            try{
-                                let i = 0;
-                                let j = 0;
-                                for(tag in data.unsubscription){
-                                    parser.get_link_details([data.unsubscription[tag].link], (newLink) => {
-                                        let unsubscribe_tag = newLink[0].link;
-                                        //console.log(`UNSUBSCRIPTION : ${unsubscribe_tag} => nothing`);
-                                        email.data = email.data.replaceAll(unsubscribe_tag, "");
-                                        i = tag;
-                                        if (j == newImageLinks.length-1 && i == data.unsubscription.length-1) {
-                                            resolve();
-                                        }
-                                    });
-                                }
-                                for(link in newImageLinks){
-                                    //console.log(`${link} : ${newImageLinks[link].oldLink} => ${newImageLinks[link].newLink}`);
-                                    email.data = email.data.replace(newImageLinks[link].oldLink, newImageLinks[link].newLink);
-                                    j = link;
-                                    if (j == newImageLinks.length-1 && i == data.unsubscription.length-1) {
-                                        resolve();
-                                    }
-                                }
-                            }catch(err){
-                                resolve();
-                                reject(err);
-                            }
-                        }).catch((err) => {console.log("UPLOAD FAILED !");});
-                        nPromise.then(() => {
-                            image.get_unique_ID((id) => {
-                                email.company.replaceAll(" ", "_");
-                                email.company.replaceAll("/", "");
-                                const filename = `src/assets/output/${email.company}${id}.html`;
-                                console.log(filename);
-                                fs.writeFile(filename, email.data, (err) => {
-                                    if (err) console.log(`Write : failed - ${err}`);
-                                });
-                            });
-                        }).catch((err) => {console.log("REPLACEMENT FAILED !");});
-                    });
-                });
-            });
-        }
-    });
-}
 */
 
-downloadAll();
+service.downloadAll();
